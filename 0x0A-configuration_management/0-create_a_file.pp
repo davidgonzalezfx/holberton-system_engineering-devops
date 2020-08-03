dba8326313852d26dc418e712d1ef3a0a91aa32a
davidonzalezfx <dvdramos16@gmail.com>
2020-08-03 20:21:07 +0000

    Feature - Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+# Puppet creates a file in /tmp
+
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
