272c26cf431393ece83426cff5f91e45daf7965b
davidonzalezfx <dvdramos16@gmail.com>
2020-08-04 23:41:40 +0000

    Fix - Avoid puppet-lint

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,5 @@
 exec { 'Install Nginx web server':
   provider => shell,
-  command  => 'apt-get -y update && \
-  apt-get -y install nginx && \
-  echo "Holberton School" > /var/www/html/index.html && \
-  echo "Ceci n\'est pas une page" > /usr/share/nginx/html/not_found.html && \
-  sed -i \'s/server_name _;/server_name _;\n\trewrite ^\/redirect_me http:\/\/davidgonzalezfx.xyz permanent;/\'\
-  /etc/nginx/sites-available default && \
-  service nginx start'
+  command  => 'apt-get -y update && apt-get -y install nginx && echo "Holberton School" > /var/www/html/index.html && echo "Ceci n\'est pas une page" > /usr/share/nginx/html/not_found.html && sed -i \'s/server_name _;/server_name _;\n\trewrite ^\/redirect_me http:\/\/davidgonzalezfx.xyz permanent;/\' /etc/nginx/sites-available default && service nginx start'
 
 }
