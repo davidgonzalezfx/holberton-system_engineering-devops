d74ece5e39d0835f561e4f555a9d849775630d02
davidgonzalezfx <dvdramos16@gmail.com>
2020-09-14 10:59:41 -0500

    Featutre - Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' Export to JSON '''
+from requests import get
+import sys
+import json
+
+if __name__ == '__main__':
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users/{}'.format(base_url, sys.argv[1])
+    res = get(user)
+    json_user = res.json()
+    username = json_user.get('username')
+
+    todos = '{}todos?userId={}'.format(base_url, sys.argv[1])
+    res = get(todos)
+    json_tasks = res.json()
+    tasks = []
+
+    for todo in json_tasks:
+        to_json = {'task': todo.get(
+            'title'), 'completed': todo.get('completed'), 'username': username}
+        tasks.append(to_json)
+
+    user_json = {str(sys.argv[1]): tasks}
+    json_file = sys.argv[1] + '.json'
+
+    with open(json_file, model='w', encoding='utf-8') as file:
+        json.dump(user_json, file)
