0d5b892c58c6389a9cbb8405630ab8f7e6accb62
davidgonzalezfx <dvdramos16@gmail.com>
2020-09-14 11:06:14 -0500

    Feature - Export to json tasks of all users

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' Export to JSON for all users'''
+from requests import get
+import sys
+import json
+
+
+if __name__ == '__main__':
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users'.format(base_url)
+    res = get(user)
+    json_user = res.json()
+
+    all_tasks = {}
+    for user in json_user:
+        username = user.get('username')
+        userid = user.get('id')
+
+        todos = '{}todos?userId={}'.format(base_url, userid)
+        res = get(todos)
+        json_tasks = res.json()
+        tasks = []
+
+        for todo in json_tasks:
+            to_json = {'username': username,
+                       'task': todo.get('title'),
+                       'completed': todo.get('completed')}
+            tasks.append(to_json)
+
+        all_tasks[str(userid)] = tasks
+
+    json_file = 'todo_all_employees.json'
+    with open(json_file, mode='w', encoding='utf-8') as file:
+        json.dump(all_tasks, file)
